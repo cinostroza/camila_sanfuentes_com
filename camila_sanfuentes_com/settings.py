d4f88bf07bceee1b831ad28bef8076a9f64a1583
cinostroza <c.inostroza@gmail.com>
2021-10-11 19:49:50 -0300

    Changes to settings.py hiding secret key.

--- camila_sanfuentes_com/settings.py
+++ camila_sanfuentes_com/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ MEDIA_URL = 'media/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+@*4$+&zb^=6&*3y)e)hkj4wxy-3(s&u7c#wfz2drv3o8cd6_3'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
