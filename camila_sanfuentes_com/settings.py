72c0ecf0f8cd58ce14a616e777aee0e9fc107fd2
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2022-12-06 13:50:27 -0300

    Cleaning up requirements.txt

--- camila_sanfuentes_com/settings.py
+++ camila_sanfuentes_com/settings.py
@@ -24,7 +24,8 @@ MEDIA_URL = 'media/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'SUPER-SECRET-KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
