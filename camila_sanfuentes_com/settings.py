9e8c69350428d63b5ca09720c39cd354a8652bac
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2022-12-10 21:52:01 -0300

    Adding changes for eb

--- camila_sanfuentes_com/settings.py
+++ camila_sanfuentes_com/settings.py
@@ -146,3 +146,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
