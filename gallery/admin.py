7052141264e8e0ec8b63000cd3c8b411db1e1da4
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2022-12-11 23:17:32 -0300

    Improving admin interface and cleaning up env variables for local development

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,9 +3,13 @@ from gallery.models import Gallery, GalleryImage
 
 # Register your models here.
 
-admin.site.register(Gallery)
-admin.site.register(GalleryImage)
-
 
 class GalleryImageInline(admin.TabularInline):
     model = GalleryImage
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    inlines = [GalleryImageInline]
+
+
+admin.site.register(Gallery, GalleryAdmin)
