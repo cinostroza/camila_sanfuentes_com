d6ec77cc9cbcd1ae8d1306c04efeb08b0210e161
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2022-12-11 22:44:16 -0300

    Customizing gallery images to show them in their galleries

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from gallery.models import Gallery, GalleryImage
+
 # Register your models here.
 
 admin.site.register(Gallery)
 admin.site.register(GalleryImage)
+
+
+class GalleryImageInline(admin.TabularInline):
+    model = GalleryImage
