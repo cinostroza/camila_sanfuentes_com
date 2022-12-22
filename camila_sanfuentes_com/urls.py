bc02faed6aaeb50da88deaad5797ef769d636ac7
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2022-12-22 10:18:18 -0300

    Adding a button to navigate to the admin site if the user is logged in

--- camila_sanfuentes_com/urls.py
+++ camila_sanfuentes_com/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
+                  path('admin/', admin.site.urls, name='admin'),
                   path('logout/', views.logout_view, name='logout'),
                   path('blog/', include('blog.urls', namespace='blog')),
                   path('subscribe/', include('contact_manager.urls', namespace='contact_manager')),
