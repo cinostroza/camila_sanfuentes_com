5f8c81889a31f647a3cd44fd7a14c371f3b5966d
Carlos Inostroza <carlosinostroz@MacBook-Pro-de-Carlos.local>
2023-01-09 11:11:36 -0300

    Modified views for gallery to use just one view so the site can handle new galleries

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,8 +5,5 @@ app_name = 'gallery'
 
 urlpatterns = [
     path('', views.GalleryIndex.as_view(), name='index'),
-    path('macro/', views.MacroGallery.as_view(), name='macro'),
-    path('paisajes/', views.PaisajeGallery.as_view(), name='paisajes'),
-    path('fauna/', views.FaunaGallery.as_view(), name='fauna'),
-    path('black_and_white/', views.BlackAndWhiteGallery.as_view(), name='black_and_white')
+    path('<str:gallery_name>', views.GalleryView.as_view(), name='gallery'),
 ]
